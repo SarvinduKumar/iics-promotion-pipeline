085684238ef7cfde24e83c3383b11c97d01fd00d
BBird <bbird@informatica.com>
2022-04-12 15:12:11 -0400

    Added testing

--- scripts/infa_login.py
+++ scripts/infa_login.py
@@ -1,11 +1,12 @@
 import requests
 import os
-from base64 import b64encode
-from nacl import encoding, public
 
 URL = os.environ['IICS_LOGIN_URL']
 USERNAME = os.environ['IICS_USERNAME']
 PASSWORD = os.environ['IICS_PASSWORD']
+
+UAT_USERNAME = os.environ['UAT_IICS_USERNAME']
+UAT_PASSWORD = os.environ['UAT_IICS_PASSWORD']
 
 URL = "https://dm-us.informaticacloud.com/saas/public/core/v3/login"
 BODY = {"username": USERNAME,"password": PASSWORD}
@@ -15,18 +16,22 @@ r = requests.post(url = URL, json = BODY)
 if r.status_code != 200:
     print("Caught exception: " + r.text)
 
+UAT_BODY = BODY = {"username": UAT_USERNAME,"password": UAT_PASSWORD}
+
+u = requests.post(url = URL, json = BODY)
+
+if u.status_code != 200:
+    print("Caught exception: " + r.text)
+
 # extracting data in json format
 data = r.json()
-
-os.environ['sessionId'] = data['userInfo']['sessionId']
-
-print(os.listdir())
+uat_data = u.json()
 
 env_file = os.getenv('GITHUB_ENV')
 
 with open(env_file, "a") as myfile:
-    myfile.write("sessionId=" + data['userInfo']['sessionId'])
-
+    myfile.write("sessionId=" + data['userInfo']['sessionId'] + "\n")
+    myfile.write("uat_sessionId=" + uat_data['userInfo']['sessionId'])
 
 #public_key = public.PublicKey(public_key.encode("utf-8"), encoding.Base64Encoder())
 #sealed_box = public.SealedBox(public_key)
---
+++ scripts/infa_update_and_test.py
@@ -0,0 +1,62 @@
+import requests
+import os
+import json
+import time
+import sys
+
+URL = os.environ['IICS_POD_URL']
+UAT_SESSION_ID = os.environ['uat_sessionId']
+UAT_COMMIT_HASH = os.environ['UAT_COMMIT_HASH']
+
+HEADERS = {"Content-Type": "application/json; charset=utf-8", "INFA-SESSION-ID": UAT_SESSION_ID }
+HEADERS_V2 = {"Content-Type": "application/json; charset=utf-8", "icSessionId": UAT_SESSION_ID }
+
+BODY={ "commitHash":UAT_COMMIT_HASH }
+
+# Sync Github and UAT Org
+p = requests.post(URL + "/public/core/v3/pullByCommitHash", headers = HEADERS, json=BODY)
+
+if p.status_code != 200:
+    print("Exception caught: " + p.text)
+    sys.exit(99)
+
+# Get all the objects for commit
+r = requests.get(URL + "/public/core/v3/commit/" + UAT_COMMIT_HASH, headers = HEADERS)
+
+if r.status_code != 200:
+    print("Exception caught: " + r.text)
+    sys.exit(99)
+    
+request_json = r.json()
+
+# Only get Mapping Tasks
+r_filtered = [x for x in request_json['changes'] if ( x['type'] == 'MTT') ]
+
+# This loop runs tests for each one of the mapping tasks
+for x in r_filtered:
+    BODY = {"@type": "job","taskId": x['appContextId'],"taskType": "MTT"}
+    t = requests.post(URL + "/api/v2/job/", headers = HEADERS_V2, json = BODY )
+
+    if t.status_code != 200:
+        print("Exception caught: " + t.text)
+        sys.exit(99)
+
+    test_json = t.json()
+    PARAMS = "?runId=" + str(test_json['runId'])
+    #"?taskId=" + test_json['taskId']
+
+    STATE=0
+    
+    while STATE == 0:
+        time.sleep(60)
+        a = requests.get(URL + "/api/v2/activity/activityLog" + PARAMS, headers = HEADERS_V2)
+        
+        activity_log = a.json()
+
+        STATE = activity_log[0]['state']
+
+    if STATE != 1:
+        print("Mapping task: " + activity_log[0]['objectName'] + " failed. ")
+        sys.exit(99)
+    else:
+        print("Mapping task: " + activity_log[0]['objectName'] + " completed successfully. ")

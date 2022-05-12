0174a92725ced71c3a981f1f297f9169cd1de2e3
BBird <bbird@informatica.com>
2022-05-12 08:20:11 -0400

    Testing more tokens

--- scripts/infa_login.py
+++ scripts/infa_login.py
@@ -38,8 +38,10 @@ env_file = os.getenv('GITHUB_ENV')
 
 with open(env_file, "a") as myfile:
     myfile.write("sessionId=" + data['userInfo']['sessionId'] + "\n")
-    myfile.write("uat_sessionId=" + uat_data['userInfo']['sessionId'])
-    myfile.write("SOME_TOKEN=Testing Masking")
+    myfile.write("uat_sessionId=" + uat_data['userInfo']['sessionId'] + "\n")
+    myfile.write("SOME_TOKEN=Testing Masking" + "\n")
+    myfile.write("::add-mask::session=Other Testing" + "\n")
+    myfile.write("session2::add-mask::=Add Testing")
 
 #public_key = public.PublicKey(public_key.encode("utf-8"), encoding.Base64Encoder())
 #sealed_box = public.SealedBox(public_key)

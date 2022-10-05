2efd2e7c380d28570216b5aa4a23bdc2a28f3e90
BBird <bbird@informatica.com>
2022-10-05 07:59:13 -0400

    Removed hardcoded url

--- scripts/helper_functions.py
+++ scripts/helper_functions.py
@@ -16,7 +16,6 @@ import json
 
 def iics_login(login_domain, iics_username, iics_password):
 
-    URL = "https://dm-us.informaticacloud.com/saas/public/core/v3/login"
     BODY = {"username": iics_username,"password": iics_password}
 
     r = requests.post(url = URL, json = BODY)
